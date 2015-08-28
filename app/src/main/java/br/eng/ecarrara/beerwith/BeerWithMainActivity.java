3c55c8f260f04d88c501141349c7f90af96bc4ea
Eduardo Carrara <ecarrara.araujo@gmail.com>
2015-08-28 09:24:01 -0300

    Basic project structure creation
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

---
+++ app/src/main/java/br/eng/ecarrara/beerwith/BeerWithMainActivity.java
@@ -0,0 +1,16 @@
+package br.eng.ecarrara.beerwith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BeerWithMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_with_main);
+    }
+
+}
---
+++ app/src/main/java/br/eng/ecarrara/beerwith/BeerWithMainActivityFragment.java
@@ -0,0 +1,22 @@
+package br.eng.ecarrara.beerwith;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BeerWithMainActivityFragment extends Fragment {
+
+    public BeerWithMainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_beer_with_main, container, false);
+    }
+}
